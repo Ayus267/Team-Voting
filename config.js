436996d4d7316266aabee2486f810fa0ba5317ac
Falstron <ayushchoudhury608@gmail.com>
2022-08-03 20:05:08 +0530

    updated

--- config.js
+++ config.js
@@ -2,8 +2,14 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyC-1CHjhfNQGdQnWvpHWNugaa0sGWl4_ow",
+    authDomain: "project-67-b09e6.firebaseapp.com",
+    projectId: "project-67-b09e6",
+    storageBucket: "project-67-b09e6.appspot.com",
+    messagingSenderId: "100555859540",
+    appId: "1:100555859540:web:f624ae51158c04dbedafc1"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
